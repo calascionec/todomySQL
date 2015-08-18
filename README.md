7281192f904c8ee1f13defd69ff92eda8b0601ff
Phillip Shannon & christopher calascione <student@epicodus.com>
2015-08-18 09:35:46 -0700

    Initial commit of todo app.

---
+++ README.md
@@ -0,0 +1,40 @@
+# To Do List with mySQL
+
+#####  A To Do list using a mySQL database, 8/17/2015
+
+#### By Ike Mattice, Phil Shannon, Jenna Navratil, Chris Calascione
+
+## Description
+
+Allows user to add items to do.  Can delete all items upon completion with one button. Same with categories.
+
+## Setup
+
+Clone this repository.
+
+Open a simplet HTTP server in the top level of the cloned directory.  For a mac, run the following command in your terminal:
+
+php -S localhost:8000
+
+Then open your web browser of choice to localhost:8000
+
+## Technologies Used
+
+HTML
+Twig
+Silex
+PHP
+mySQL
+PHPUnit
+
+### Legal
+
+Copyright (c) 2015 Ike Mattice, Jenna Navratil, Phil Shannon, Chris Calascione
+
+This software is licensed under the MIT license.
+
+Permission is hereby granted, on a donation basis, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ tests/TaskTest.php
@@ -0,0 +1,92 @@
+<?php
+     /**
+     * @backupGlobals disabled
+     * @backupStaticAttributes disabled
+     */
+
+     require_once "src/Task.php";
+
+     $server = 'mysql:host=localhost;dbname=to_do_test';
+     $username = 'root';
+     $password = 'root';
+     $DB = new PDO($server, $username, $password);
+
+     class TaskTest extends PHPUnit_Framework_TestCase
+     {
+
+         protected function tearDown()
+         {
+             Task::deleteAll();
+         }
+
+         function test_save()
+         {
+             //Arrange
+             $description = "Wash the dog";
+             $test_task = new Task($description);
+
+             //Act
+             $test_task->save();
+
+             //Assert
+             $result = Task::getAll();
+             $this->assertEquals($test_task, $result[0]);
+         }
+
+         function test_getAll()
+         {
+             $description = "Wash the dog";
+             $description2 = "Water the lawn";
+             $test_Task = new Task ($description);
+             $test_Task->save();
+             $test_Task2 = new Task ($description2);
+             $test_Task2-> save();
+
+             $result = Task::getAll();
+
+             $this->assertEquals([$test_Task, $test_Task2], $result);
+         }
+
+         function test_deleteAll()
+         {
+             $description = "Wash the dog";
+             $description2 = "Water the lawn";
+             $test_Task = new Task($description);
+             $test_Task->save();
+             $test_Task2 = new Task($description2);
+             $test_Task2->save();
+
+             Task::deleteAll();
+
+             $result = Task::getAll();
+             $this->assertEquals([], $result);
+         }
+
+         function test_getId()
+         {
+             $description = "Wash the dog";
+             $id = 1;
+             $test_Task = new Task($description, $id);
+
+             $result = $test_Task->getId();
+
+             $this->assertEquals(1, $result);
+         }
+
+         function test_find()
+         {
+             $description = "Wash the Dog";
+             $description2 = "Water the Lawn";
+             $test_Task = new Task ($description);
+             $test_Task->save();
+             $test_Task2 = new Task ($description2);
+             $test_Task2->save();
+
+             $id = $test_Task->getId();
+             $result = Task::find($id);
+
+             $this->assertEquals($test_Task, $result);
+         }
+     }
+
+?>
